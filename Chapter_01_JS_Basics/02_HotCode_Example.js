7ccd01da7163d73498cc53df317b9a763b788222
mansi <mansibhadoria2@gmail.com>
2026-02-19 01:12:19 +0530

    chore: initial import of LearningPlaywright examples

---
+++ Chapter_01_JS_Basics/02_HotCode_Example.js
@@ -0,0 +1,7 @@
+let a = 10;
+console.log(a);  //not a Hot Code
+
+//Hot Code
+for (let i = 0; i < 10000; i++) {
+    console.log(i);
+}
---
+++ Chapter_01_JS_Basics/03_platform_verification.js
@@ -0,0 +1,2 @@
+console.log("Platform Version :" + process.platform);
+console.log("Node Version: " + process.version);
