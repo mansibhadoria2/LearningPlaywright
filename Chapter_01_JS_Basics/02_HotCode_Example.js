70576599de953380d4696a5ea5ad294efe4dd54e
mansi <mansibhadoria2@gmail.com>
2026-02-19 23:08:21 +0530

    Update Chapter_01 HotCode example

--- Chapter_01_JS_Basics/02_HotCode_Example.js
+++ Chapter_01_JS_Basics/02_HotCode_Example.js
@@ -2,6 +2,16 @@ let a = 10;
 console.log(a);  //not a Hot Code
 
 //Hot Code
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 15000; i++) {
     console.log(i);
 }
+console.log("Addition")
+function add(a, b) {
+    return a + b;
+}
+let result;
+for (let i = 0; i < 10000; i++) {
+    result = add(i, i + 1);
+
+}
+console.log("After 10000 calls: ", +result);
